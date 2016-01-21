46873bd6151e2d14610d7436b80dfc3730d17733
Blah-blah <mithunksingh98@gmail.com>
2016-01-21 20:59:28 +0530

    Bad indenttion.

--- js/index.js
+++ js/index.js
@@ -61,9 +61,9 @@ var Gamecenter = Class(function () {
 		return;
 	};
 
-    this.showGameCenter = function() {
-      pluginSend("showGameCenter");
-    };
+        this.showGameCenter = function() {
+          pluginSend("showGameCenter");
+        };
 });
 
 exports = new Gamecenter();
